3b6570d9539611ec7e3899aa16b8c596584614c0
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-06-07 14:48:34 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,7 +12,7 @@ beneficiary = pd.read_csv("benificiary_d.csv")
 
 
 st.set_page_config(page_title="Demographic Distribution of CMS Beneficiary Data", layout="wide")
-st.markdown(f"<h1 style='text-align: center; color: #35BAE2 ; width:1360px;height : 100px '>Demographic Distribution of CMS Beneficiary Data</h1>",unsafe_allow_html=True)
+st.markdown(f"<h1 style='text-align: left; color: #35BAE2 ; width:1360px;height : 100px '>Demographic Distribution of CMS Beneficiary Data</h1>",unsafe_allow_html=True)
 
 
 
