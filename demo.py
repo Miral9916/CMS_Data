bc3db043127bbe20342f8f28d1e2e1bf96f8ee6e
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-06-07 15:09:47 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,7 +17,7 @@ st.markdown(f"<h1 style='text-align: left; color: #00008B; width:1360px; height 
 with st.sidebar:
     st.markdown(
         '<div style="background-color: #00008B; height: 50px; width: 298px; border-radius: 5px">'
-        '<h2 style = "text-align: center;display: flex; align-items: center;color: white">Filter</h2>'
+        '<h2 style = "text-align: center; color: white">Filter</h2>'
         '</div>', unsafe_allow_html=True
     )
 
