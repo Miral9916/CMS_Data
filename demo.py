f7026d4525fde9518ffa1f1302a8101e94a317a7
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-14 15:09:16 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -137,22 +137,22 @@ base_raffronto = alt.Chart(df_raffronto).encode(
     x=alt.X('month(data):O', axis=alt.Axis(title='Month')),
 )
 
-bar_tamponi = base_raffronto.mark_bar(opacity=.4, color='orange').encode(
-    alt.Tooltip(['data:T', 'Scostamento tamponi:Q']),
-    y=alt.Y('Scostamento tamponi:Q', axis=alt.Axis(title='COVID Hospitalization Rate (%)')),
-)
+# bar_tamponi = base_raffronto.mark_bar(opacity=.4, color='orange').encode(
+#     alt.Tooltip(['data:T', 'Scostamento tamponi:Q']),
+#     y=alt.Y('Scostamento tamponi:Q', axis=alt.Axis(title='COVID Hospitalization Rate (%)')),
+# )
 
-bar_nuovi_pos = base_raffronto.mark_bar(opacity=.4, color='blue').encode(
-    alt.Tooltip(['data:T', 'nuovi_positivi:Q']),
-    y=alt.Y('nuovi_positivi:Q', axis=alt.Axis(title='All Hospitalization Rate (%)')),
-)
+# bar_nuovi_pos = base_raffronto.mark_bar(opacity=.4, color='blue').encode(
+#     alt.Tooltip(['data:T', 'nuovi_positivi:Q']),
+#     y=alt.Y('nuovi_positivi:Q', axis=alt.Axis(title='All Hospitalization Rate (%)')),
+# )
 
-line_perc = base_raffronto.mark_line(opacity=.7, color='red', point=True).encode(
-    tooltip=[{'type': 'temporal', 'field': 'data'}, {'type': 'quantitative', 'field': 'perc_positivi', 'format': '.2f'}],
-    y=alt.Y('perc_positivi:Q', axis=alt.Axis(title='BA.2 Variant Proportion')),
-)
+# line_perc = base_raffronto.mark_line(opacity=.7, color='red', point=True).encode(
+#     tooltip=[{'type': 'temporal', 'field': 'data'}, {'type': 'quantitative', 'field': 'perc_positivi', 'format': '.2f'}],
+#     y=alt.Y('perc_positivi:Q', axis=alt.Axis(title='BA.2 Variant Proportion')),
+# )
 
-layer_bars = alt.layer(bar_tamponi, bar_nuovi_pos)
-st.altair_chart(alt.layer(layer_bars, line_perc).resolve_scale(y='independent').properties(width=650, height=400).interactive())
+# layer_bars = alt.layer(bar_tamponi, bar_nuovi_pos)
+# st.altair_chart(alt.layer(layer_bars, line_perc).resolve_scale(y='independent').properties(width=650, height=400).interactive())
 
 
